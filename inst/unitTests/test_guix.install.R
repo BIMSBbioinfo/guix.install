5b1135686bb17d6e2547121aa7721f8b491bc1be
Ricardo Wurmus <rekado@elephly.net>
2022-04-12 14:57:08 +0200

    tests: Use cacheFile.

--- inst/unitTests/test_guix.install.R
+++ inst/unitTests/test_guix.install.R
@@ -1,5 +1,5 @@
 test.guix.install <- function () {
-    checkException(guix.install("foo", guix="/doesnot/exist"))
+    checkException(guix.install("foo", guix="/doesnot/exist", cacheFile=tempfile()))
 
     msg <- geterrmessage()
     checkTrue(grepl("Failed to run Guix command", msg))
